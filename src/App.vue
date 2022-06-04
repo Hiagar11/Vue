19a68eb32c5b53aa9c54a72d3bfd729a0503cad8
Hiagar11 <hiagar11@gmail.com>
2022-06-04 10:57:13 +0300

    Почистили сборку перед обучением

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <div>Hi Vue how are you?</div>
-  <v-main-wrapper />
+
 </template>
 
 <script>
-import vMainWrapper from './components/v-main-wrapper'
+
 
 export default {
   name: 'App',
   components: {
-    vMainWrapper
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-};
+
 </style>
